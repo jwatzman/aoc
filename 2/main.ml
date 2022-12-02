9d85ddb222b7e6e982c3f62f9ee063c42687637f
Josh Watzman <jwatzman@cord.com>
2022-12-02 09:57:11 +0000

    2.2

--- 2/main.ml
+++ 2/main.ml
@@ -1,33 +1,38 @@
 type rps = Rock | Paper | Scissors
+type result = Win | Loss | Draw
 
-let fixed_xyz = (Rock, Paper, Scissors)
-let parse_line line (x, y, z) =
+let parse_line line =
   let first = match line.[0] with
     | 'A' -> Rock
     | 'B' -> Paper
     | 'C' -> Scissors
     | _ -> failwith "Invalid first char" in
   let second = match line.[2] with
-    | 'X' -> x
-    | 'Y' -> y
-    | 'Z' -> z
+    | 'X' -> Loss
+    | 'Y' -> Draw
+    | 'Z' -> Win
     | _ -> failwith "Invalid second char" in
   (first, second)
 
-type result = Win | Loss | Draw
-let find_result = function
-    | (them, us) when them = us -> Draw
-    | (Rock, Paper) -> Win
-    | (Paper, Scissors) -> Win
-    | (Scissors, Rock) -> Win
-    | _ -> Loss
+let find_play = function
+  | (them, Draw) -> them
+  | (them, Win) -> (match them with
+    | Rock -> Paper
+    | Paper -> Scissors
+    | Scissors -> Rock)
+  | (them, Loss) -> (match them with
+    | Rock -> Scissors
+    | Paper -> Rock
+    | Scissors -> Paper)
 
-let score ((_, second) as t) =
-  let win_score = match find_result t with
+let result_to_play ((_, result) as t) = (result, find_play t)
+
+let score (result, us) =
+  let win_score = match result with
     | Win -> 6
     | Draw -> 3
     | Loss -> 0 in
-  let choice_score = match second with
+  let choice_score = match us with
     | Rock -> 1
     | Paper -> 2
     | Scissors -> 3 in
@@ -38,7 +43,7 @@ let tot_score = List.fold_left (fun s t -> s + score t) 0
 let parse_stream ic =
   let rec parse_stream_helper ls =
     try
-      let l = parse_line (input_line ic) fixed_xyz in
+      let l = parse_line (input_line ic) in
       parse_stream_helper (l::ls)
     with End_of_file ->
       ls
@@ -48,7 +53,8 @@ let parse_stream ic =
 let main () =
   let ic = open_in Sys.argv.(1) in
   let parsed = parse_stream ic in
-  let score = tot_score parsed in
+  let with_plays = List.map result_to_play parsed in
+  let score = tot_score with_plays in
   print_endline (string_of_int score);
   ()
 
