c5c94eca0c8137306a2708213d5783d75acbc764
Josh Watzman <jwatzman@cord.com>
2022-12-06 08:45:30 +0000

    6.2

--- 6/6.py
+++ 6/6.py
@@ -4,9 +4,11 @@ import sys
 
 f = open(sys.argv[1], "r")
 line = f.readlines()[0]
-i = 4
+
+uniq = 14
+i = uniq
 while i <= len(line):
-	if len(set(line[i-4:i])) == 4:
+	if len(set(line[i-uniq:i])) == uniq:
 		print(i)
 		sys.exit(0)
 	i += 1
