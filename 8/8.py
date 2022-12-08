76772ab52e28e36300c1f15f4b358d0a2c24494c
Josh Watzman <jwatzman@cord.com>
2022-12-08 09:43:06 +0000

    8.2

--- 8/8.py
+++ 8/8.py
@@ -14,37 +14,27 @@ heights = list(map(parse_line, f.readlines()))
 MAX_I = len(heights)
 MAX_J = len(heights[0])
 
-can_see = [[False for j in range(MAX_J)] for i in range(MAX_I)]
+scenic_score = [[None for j in range(MAX_J)] for i in range(MAX_I)]
 
-def check(i, j, maxheight):
+def scan(i, j, deltai, deltaj):
+	tot = 0
 	height = heights[i][j]
-	if height > maxheight:
-		can_see[i][j] = True
-		return height
-	return maxheight
-
+	i += deltai
+	j += deltaj
+	while i >= 0 and i < MAX_I and j >= 0 and j < MAX_J:
+		tot += 1
+		if heights[i][j] >= height:
+			break
+		i += deltai
+		j += deltaj
+	return tot
 
 for i in range(MAX_I):
-	maxheight = -1
 	for j in range(MAX_J):
-		maxheight = check(i, j, maxheight)
-	maxheight = -1
-	for j in reversed(range(MAX_J)):
-		maxheight = check(i, j, maxheight)
+		a = scan(i, j, 1, 0)
+		b = scan(i, j, -1, 0)
+		c = scan(i, j, 0, 1)
+		d = scan(i, j, 0, -1)
+		scenic_score[i][j] = a*b*c*d
 
-for j in range(MAX_J):
-	maxheight = -1
-	for i in range(MAX_I):
-		maxheight = check(i, j, maxheight)
-	maxheight = -1
-	for i in reversed(range(MAX_I)):
-		maxheight = check(i, j, maxheight)
-
-#print(can_see)
-
-tot = 0
-for i in range(MAX_I):
-	for j in range(MAX_J):
-		if can_see[i][j]:
-			tot += 1
-print(tot)
+print(max(map(max, scenic_score)))
