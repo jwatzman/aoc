10ff970b20c3013dbf538d2a7f50f62a6e854118
Josh Watzman <jwatzman@cord.com>
2022-12-13 10:43:59 +0000

    13.2

--- 13/main.ml
+++ 13/main.ml
@@ -31,13 +31,13 @@ let parse_packet ic =
 	let p2 = parse_line (input_line ic) in
 	(p1, p2)
 
-let rec parse_stream ic n =
-	let p = (parse_packet ic, n) in
+let rec parse_stream ic =
+	let (p1, p2) = parse_packet ic in
 	try
 		let _ = input_line ic in
-		p::(parse_stream ic (n+1))
+		p1::p2::(parse_stream ic)
 	with End_of_file ->
-		[p]
+		[p1; p2]
 
 let ensure_list elem = match elem with
 	| List _ -> elem
@@ -56,20 +56,29 @@ and compare_lists = function
 	| (_, []) -> Some false
 	| ([], _) -> Some true
 
-let rec tally = function
-	| [] -> 0
-	| (p, n)::rest -> 
-		let r = tally rest in
-		match compare_elems p with
-			| Some true -> n + r
-			| Some false -> r
-			| None -> failwith "Did not compare"
+let cmp e1 e2 = match compare_elems (e1, e2) with
+	| None -> 0
+	| Some true -> -1
+	| Some false -> 1
+
+let d1 = parse_line "[[2]]"
+let d2 = parse_line "[[6]]"
+
+let rec do_find n nd1 nd2 = function
+	| [] -> (nd1, nd2)
+	| (List [List [Num 2]])::ps -> do_find (n+1) n nd2 ps
+	| (List [List [Num 6]])::ps -> do_find (n+1) nd1 n ps
+	| _::ps -> do_find (n+1) nd1 nd2 ps
 
 let main () =
   let ic = open_in Sys.argv.(1) in
-	let ps = parse_stream ic 1 in
-	let n = tally ps in
-	let () = print_endline (string_of_int n) in
+	let ps = parse_stream ic in
+	let ps = d1::d2::ps in
+	let ps = List.sort cmp ps in
+	let (nd1, nd2) = do_find 1 0 0 ps in
+	let () = print_endline (string_of_int nd1) in
+	let () = print_endline (string_of_int nd2) in
+	let () = print_endline (string_of_int (nd1*nd2)) in
 	()
 
 let () = main ()
