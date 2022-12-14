09caa3ba8d99718fe67db329ae80be33d6852c31
Josh Watzman <jwatzman@cord.com>
2022-12-14 16:56:32 +0000

    14.2

--- 14/aoc14.py
+++ 14/aoc14.py
@@ -20,18 +20,16 @@ class SandMap:
 	def drop(self, x, y):
 		assert self.maxy != None
 		assert not self.contains(x, y)
-		#print(x, y)
-		if y > self.maxy:
-			return True
+		if y == self.maxy + 1:
+			self.dict[x].add(y)
 		elif not self.contains(x, y+1):
-			return self.drop(x, y+1)
+			self.drop(x, y+1)
 		elif not self.contains(x-1, y+1):
-			return self.drop(x-1, y+1)
+			self.drop(x-1, y+1)
 		elif not self.contains(x+1, y+1):
-			return self.drop(x+1, y+1)
+			self.drop(x+1, y+1)
 		else:
-			self.block(x, y)
-			return False
+			self.dict[x].add(y)
 
 def all_points(x1, y1, x2, y2):
 	r = []
@@ -65,6 +63,7 @@ for line in f.readlines():
 	block_all(m, points)
 
 dropped = 0
-while not m.drop(500,0):
+while not m.contains(500,0):
+	m.drop(500,0)
 	dropped += 1
 print(dropped)
