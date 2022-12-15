69386de3fe1cfd6fcad4b5f486530743d75a603e
Josh Watzman <jwatzman@cord.com>
2022-12-15 15:06:38 +0000

    15.1.5

--- 15/15.py
+++ 15/15.py
@@ -5,8 +5,47 @@ import sys
 
 r = re.compile("Sensor at x=(.+), y=(.+): closest beacon is at x=(.+), y=(.+)")
 targety = int(sys.argv[2])
-hitx = set()
 bhitx = set()
+
+hitranges = []
+
+class Rng:
+	def __init__(self, l, r):
+		self.l = l
+		self.r = r
+
+	def __str__(self):
+		return "range(" + str(self.l) + "," + str(self.r) + ")"
+
+def mergeinto(rng, l, r):
+	if l >= rng.l and r <= rng.r:
+		return True
+	if l < rng.l and r <= rng.r and r >= rng.l:
+		rng.l = l
+		return True
+	if l > rng.l and r >= rng.r and l <= rng.r:
+		rng.r = r
+		return True
+	if l < rng.l and r > rng.r:
+		rng.l = l
+		rng.r = r
+		return True
+	return False
+
+def addrange(l, r):
+	for rng in hitranges:
+		if mergeinto(rng, l, r):
+			return
+	hitranges.append(Rng(l, r))
+
+def flattenranges(l):
+	for rng1 in l:
+		for rng2 in l:
+			if rng1 == rng2:
+				continue
+			if mergeinto(rng1, rng2.l, rng2.r):
+				rng2.l = 0
+				rng2.r = 0
 
 f = open(sys.argv[1], "r")
 for line in f.readlines():
@@ -22,9 +61,12 @@ for line in f.readlines():
 	steps = abs(sx-bx) + abs(sy-by)
 	if abs(sy-targety) <= steps:
 		steps -= abs(sy-targety)
-		hitx.add(sx)
-		for dx in range(steps):
-			hitx.add(sx+dx+1)
-			hitx.add(sx-dx-1)
+		addrange(sx-steps-1, sx+steps+1)
 
-print(len(hitx) - len(bhitx))
+tot = 0
+flattenranges(hitranges)
+for rng in hitranges:
+	tot += rng.r - rng.l
+tot -= len(bhitx)
+
+print(tot - 1)
