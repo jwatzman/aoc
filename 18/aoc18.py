9f9d881117841fad59bd318c4a397ef14e6eba41
Josh Watzman <github@jwatzman.org>
2022-12-18 10:09:37 +0000

    18.1.5

--- 18/aoc18.py
+++ 18/aoc18.py
@@ -12,15 +12,12 @@ maxz = None
 
 def raycast(x, y, z, dx, dy, dz):
 	while x >= minx and x <= maxx and y >= miny and y <= maxy and z >= minz and z <= maxz:
-		if (x,y,z) in droplets:
-			return 1
 		x += dx
 		y += dy
 		z += dz
+		if (x,y,z) in droplets:
+			return 1
 	return 0
-
-def inclrange(l, r):
-	return range(l, r+1)
 
 f = open(sys.argv[1], "r")
 for line in f.readlines():
@@ -42,19 +39,17 @@ for line in f.readlines():
 	if maxz == None or z > maxz:
 		maxz = z
 
-hits = 0
-for x in inclrange(minx, maxx):
-	for y in inclrange(miny, maxy):
-		hits += raycast(x,y,minz, 0,0,1)
-		hits += raycast(x,y,maxz, 0,0,-1)
+adj = 0
+for (x,y,z) in droplets:
+	adj += raycast(x,y,z, 1,0,0)
+	adj += raycast(x,y,z, -1,0,0)
+	adj += raycast(x,y,z, 0,1,0)
+	adj += raycast(x,y,z, 0,-1,0)
+	adj += raycast(x,y,z, 0,0,1)
+	adj += raycast(x,y,z, 0,0,-1)
 
-	for z in inclrange(minz, maxz):
-		hits += raycast(x,miny,z, 0,1,0)
-		hits += raycast(x,maxy,z, 0,-1,0)
-
-for y in inclrange(miny, maxy):
-	for z in inclrange(minz, maxz):
-		hits += raycast(minx,y,z, 1,0,0)
-		hits += raycast(maxx,y,z, -1,0,0)
-
-print(hits)
+print(minx, maxx)
+print(miny, maxy)
+print(minz, maxz)
+print(adj)
+print(6*len(droplets) - adj)
