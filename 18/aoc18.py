464c2d0e06517e124210a1d9360d29e79a02ebe9
Josh Watzman <github@jwatzman.org>
2022-12-18 11:02:12 +0000

    18.2

--- 18/aoc18.py
+++ 18/aoc18.py
@@ -1,55 +1,80 @@
 #!/usr/bin/env python3
 
+from dataclasses import dataclass
 import sys
 
-droplets = set()
-minx = None
-maxx = None
-miny = None
-maxy = None
-minz = None
-maxz = None
-
-def raycast(x, y, z, dx, dy, dz):
-	while x >= minx and x <= maxx and y >= miny and y <= maxy and z >= minz and z <= maxz:
-		x += dx
-		y += dy
-		z += dz
-		if (x,y,z) in droplets:
-			return 1
-	return 0
-
+xs = []
+ys = []
+zs = []
+rocks = set()
 f = open(sys.argv[1], "r")
 for line in f.readlines():
 	coords = line.rstrip().split(",")
-	coords = list(map(int, coords))
-	(x,y,z) = coords
-	droplets.add((x,y,z))
-	if minx == None or x < minx:
-		minx = x
-	if miny == None or y < miny:
-		miny = y
-	if minz == None or z < minz:
-		minz = z
+	(x,y,z) = map(int, coords)
+	xs.append(x)
+	ys.append(y)
+	zs.append(z)
+	rocks.add((x,y,z))
 
-	if maxx == None or x > maxx:
-		maxx = x
-	if maxy == None or y > maxy:
-		maxy = y
-	if maxz == None or z > maxz:
-		maxz = z
+minx = min(xs) - 1
+maxx = max(xs) + 1
+miny = min(ys) - 1
+maxy = max(ys) + 1
+minz = min(zs) - 1
+maxz = max(zs) + 1
+
+@dataclass
+class Void:
+	x: int
+	y: int
+	z: int
+	comp: int = None
+
+def inclrange(l,r):
+	return range(l, r+1)
+
+voids = dict()
+for x in inclrange(minx, maxx):
+	for y in inclrange(miny, maxy):
+		for z in inclrange(minz, maxz):
+			if not (x,y,z) in rocks:
+				voids[(x,y,z)] = Void(x,y,z)
+
+bordercomp = None
+
+def succs(x,y,z):
+	return [(x+1,y,z), (x-1,y,z), (x,y+1,z), (x,y-1,z), (x,y,z+1), (x,y,z-1)]
+
+def flood(void, ncomp):
+	global voids
+	global bordercomp
+	if void.comp != None:
+		return
+	void.comp = ncomp
+	for (sx,sy,sz) in succs(void.x, void.y, void.z):
+		if sx < minx or sx > maxx or sy < miny or sy > maxy or sz < minz or sz > maxz:
+			assert bordercomp == None or bordercomp == ncomp
+			bordercomp = ncomp
+		elif (sx,sy,sz) in voids:
+			flood(voids[(sx,sy,sz)], ncomp)
+
+ncomp = 1
+sys.setrecursionlimit(len(voids) + 50)
+for loc in voids:
+	void = voids[loc]
+	flood(void, ncomp)
+	ncomp += 1
+
+for loc in voids:
+	void = voids[loc]
+	assert void.comp != None
+	if void.comp != bordercomp:
+		rocks.add(loc)
 
 adj = 0
-for (x,y,z) in droplets:
-	adj += raycast(x,y,z, 1,0,0)
-	adj += raycast(x,y,z, -1,0,0)
-	adj += raycast(x,y,z, 0,1,0)
-	adj += raycast(x,y,z, 0,-1,0)
-	adj += raycast(x,y,z, 0,0,1)
-	adj += raycast(x,y,z, 0,0,-1)
+for (x,y,z) in rocks:
+	for succ in succs(x,y,z):
+		if succ in rocks:
+			adj += 1
 
-print(minx, maxx)
-print(miny, maxy)
-print(minz, maxz)
-print(adj)
-print(6*len(droplets) - adj)
+print(6*len(rocks) - adj)
