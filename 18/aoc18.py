a302a6d9cb61dfd8fdb594baa67b31d31ebf3174
Josh Watzman <github@jwatzman.org>
2022-12-18 10:03:17 +0000

    18.1.5

--- 18/aoc18.py
+++ 18/aoc18.py
@@ -3,26 +3,58 @@
 import sys
 
 droplets = set()
+minx = None
+maxx = None
+miny = None
+maxy = None
+minz = None
+maxz = None
 
-def checkadj(x, y, z, dx, dy, dz):
-	if ((x+dx), (y+dy), (z+dz)) in droplets:
-		return 1
-	else:
-		return 0
+def raycast(x, y, z, dx, dy, dz):
+	while x >= minx and x <= maxx and y >= miny and y <= maxy and z >= minz and z <= maxz:
+		if (x,y,z) in droplets:
+			return 1
+		x += dx
+		y += dy
+		z += dz
+	return 0
+
+def inclrange(l, r):
+	return range(l, r+1)
 
 f = open(sys.argv[1], "r")
 for line in f.readlines():
 	coords = line.rstrip().split(",")
 	coords = list(map(int, coords))
-	droplets.add((coords[0], coords[1], coords[2]))
+	(x,y,z) = coords
+	droplets.add((x,y,z))
+	if minx == None or x < minx:
+		minx = x
+	if miny == None or y < miny:
+		miny = y
+	if minz == None or z < minz:
+		minz = z
 
-adj = 0
-for (x,y,z) in droplets:
-	adj += checkadj(x,y,z, 1,0,0)
-	adj += checkadj(x,y,z, -1,0,0)
-	adj += checkadj(x,y,z, 0,1,0)
-	adj += checkadj(x,y,z, 0,-1,0)
-	adj += checkadj(x,y,z, 0,0,1)
-	adj += checkadj(x,y,z, 0,0,-1)
+	if maxx == None or x > maxx:
+		maxx = x
+	if maxy == None or y > maxy:
+		maxy = y
+	if maxz == None or z > maxz:
+		maxz = z
 
-print(6*len(droplets) - adj)
+hits = 0
+for x in inclrange(minx, maxx):
+	for y in inclrange(miny, maxy):
+		hits += raycast(x,y,minz, 0,0,1)
+		hits += raycast(x,y,maxz, 0,0,-1)
+
+	for z in inclrange(minz, maxz):
+		hits += raycast(x,miny,z, 0,1,0)
+		hits += raycast(x,maxy,z, 0,-1,0)
+
+for y in inclrange(miny, maxy):
+	for z in inclrange(minz, maxz):
+		hits += raycast(minx,y,z, 1,0,0)
+		hits += raycast(maxx,y,z, -1,0,0)
+
+print(hits)
