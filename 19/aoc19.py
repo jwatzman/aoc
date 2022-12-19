cfcc310ba1aaca80955d53753e5f668c3a08352e
Josh Watzman <github@jwatzman.org>
2022-12-19 10:53:36 +0000

    19.1.x

--- 19/aoc19.py
+++ 19/aoc19.py
@@ -16,7 +16,6 @@ class Blueprint:
 
 @dataclass(frozen=True)
 class State:
-	blueprint: Blueprint
 	ore: int
 	clay: int
 	obs: int
@@ -26,25 +25,27 @@ class State:
 	obsrob: int
 	gerob: int
 
-	def step(self, accum):
+	def step(self, blueprint, accum):
 		newore = self.ore + self.orerob
 		newclay = self.clay + self.clayrob
 		newobs = self.obs + self.obsrob
 		newge = self.ge + self.gerob
-		accum.add(State(self.blueprint, newore, newclay, newobs, newge, self.orerob, self.clayrob, self.obsrob, self.gerob))
-		if self.ore >= self.blueprint.orerob_ore:
-			accum.add(State(self.blueprint, newore - blueprint.orerob_ore, newclay, newobs, newge, self.orerob + 1, self.clayrob, self.obsrob, self.gerob))
-		if self.ore >= self.blueprint.clayrob_ore:
-			accum.add(State(self.blueprint, newore - blueprint.clayrob_ore, newclay, newobs, newge, self.orerob, self.clayrob + 1, self.obsrob, self.gerob))
-		if self.ore >= self.blueprint.obsrob_ore and self.clay >= self.blueprint.obsrob_clay:
-			accum.add(State(self.blueprint, newore - blueprint.obsrob_ore, newclay - self.blueprint.obsrob_clay, newobs, newge, self.orerob, self.clayrob, self.obsrob + 1, self.gerob))
-		if self.ore >= self.blueprint.gerob_ore and self.obs >= self.blueprint.gerob_obs:
-			accum.add(State(self.blueprint, newore - blueprint.gerob_ore, newclay, newobs - self.blueprint.gerob_obs, newge, self.orerob, self.clayrob, self.obsrob, self.gerob + 1))
+		accum.add(State(newore, newclay, newobs, newge, self.orerob, self.clayrob, self.obsrob, self.gerob))
+		if self.ore >= blueprint.orerob_ore:
+			accum.add(State(newore - blueprint.orerob_ore, newclay, newobs, newge, self.orerob + 1, self.clayrob, self.obsrob, self.gerob))
+		if self.ore >= blueprint.clayrob_ore:
+			accum.add(State(newore - blueprint.clayrob_ore, newclay, newobs, newge, self.orerob, self.clayrob + 1, self.obsrob, self.gerob))
+		if self.ore >= blueprint.obsrob_ore and self.clay >= blueprint.obsrob_clay:
+			accum.add(State(newore - blueprint.obsrob_ore, newclay - blueprint.obsrob_clay, newobs, newge, self.orerob, self.clayrob, self.obsrob + 1, self.gerob))
+		if self.ore >= blueprint.gerob_ore and self.obs >= blueprint.gerob_obs:
+			accum.add(State(newore - blueprint.gerob_ore, newclay, newobs - blueprint.gerob_obs, newge, self.orerob, self.clayrob, self.obsrob, self.gerob + 1))
 
-def stepall(accum):
+def stepall(blueprint, accum):
 	r = set()
-	for state in accum:
-		state.step(r)
+	while accum:
+		state = accum.pop()
+		state.step(blueprint, r)
+		del state
 	return r
 
 r = re.compile("Blueprint (.+): Each ore robot costs (.+) ore. Each clay robot costs (.+) ore. Each obsidian robot costs (.+) ore and (.+) clay. Each geode robot costs (.+) ore and (.+) obsidian.")
@@ -59,9 +60,16 @@ for line in f.readlines():
 for blueprint in blueprints:
 	print(blueprint)
 	states = set()
-	states.add(State(blueprint, 0, 0, 0, 0, 1, 0, 0, 0))
+	states.add(State(0, 0, 0, 0, 1, 0, 0, 0))
 	for s in range(24):
 		print(s, end=" ", flush=True)
-		states = stepall(states)
-	m = max(map(lambda s: s.ge, states))
+		newstates = stepall(blueprint, states)
+		del states
+		states = newstates
+	m = 0
+	while states:
+		s = states.pop()
+		if s.ge > m:
+			m = s.ge
+		del s
 	print("-> ", m)
