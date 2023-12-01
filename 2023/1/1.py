cf2b27adb31643f774f1cbacf8f5e1e24e6609d8
Josh Watzman <github@jwatzman.org>
2023-12-01 18:15:00 +0000

    1.2

--- 2023/1/1.py
+++ 2023/1/1.py
@@ -1,19 +1,42 @@
 #!/usr/bin/env python3
 
+import regex as re
 import sys
 
+def tonum(s):
+	if s == "one":
+		return 1
+	elif s == "two":
+		return 2
+	elif s == "three":
+		return 3
+	elif s == "four":
+		return 4
+	elif s == "five":
+		return 5
+	elif s == "six":
+		return 6
+	elif s == "seven":
+		return 7
+	elif s == "eight":
+		return 8
+	elif s == "nine":
+		return 9
+	else:
+		return int(s)
+
+reg = re.compile('(?:one)|(?:two)|(?:three)|(?:four)|(?:five)|(?:six)|(?:seven)|(?:eight)|(?:nine)|1|2|3|4|5|6|7|8|9')
+
 f = open(sys.argv[1], "r")
+
 tot = 0
 for line in f.readlines():
-	n = 0
-	for char in line:
-		if char.isdigit():
-			n = 10 * int(char)
-			break
-	for char in line[::-1]:
-		if char.isdigit():
-			n += int(char)
-			break
-	tot += n
+	matches = reg.findall(line, overlapped=True)
+	print(line)
+	print(matches[0])
+	print(matches[-1])
+	print(10 * tonum(matches[0]) + tonum(matches[-1]))
+	print('---')
+	tot += 10 * tonum(matches[0]) + tonum(matches[-1])
 
 print(tot)
