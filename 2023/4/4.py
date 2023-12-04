31631a7b5d514271a7f7ae762877b1f9d885d629
Josh Watzman <github@jwatzman.org>
2023-12-04 19:01:46 +0000

    4.2

--- 2023/4/4.py
+++ 2023/4/4.py
@@ -11,20 +11,30 @@ def mk_set(s):
 			res.add(int(n))
 	return res
 
-def score(n):
-	if n == 0:
-		return 0
-	else:
-		return 2 ** (n-1)
-
 f = open(sys.argv[1], "r")
 
-tot = 0
+card_matchers = dict()
+max_card_num = 0
 for line in f.readlines():
 	line_split = line.split(":")
+	card_num = int(line_split[0][4:])
 	numbers_split = line_split[1].strip().split("|")
 	winning_numbers_set = mk_set(numbers_split[0].strip())
 	my_numbers_set = mk_set(numbers_split[1].strip())
 	i = winning_numbers_set.intersection(my_numbers_set)
-	tot += score(len(i))
+	card_matchers[card_num] = len(i)
+	max_card_num = card_num
+
+cards_cache = dict()
+def cards(card_num):
+	if card_num not in cards_cache:
+		ret = card_matchers[card_num]
+		for i in range(ret):
+			ret += cards(card_num + i + 1)
+		cards_cache[card_num] = ret
+	return cards_cache[card_num]
+
+tot = 0
+for i in range(max_card_num):
+	tot += cards(i + 1) + 1
 print(tot)
