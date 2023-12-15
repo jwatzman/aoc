67548daefaf62fb8b45381d37147d98507bf7f76
Josh Watzman <github@jwatzman.org>
2023-12-15 18:24:27 +0000

    15.2

--- 2023/15/15.py
+++ 2023/15/15.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import re
 import sys
 
 def hashalgo(s):
@@ -13,7 +14,35 @@ def hashalgo(s):
 f = open(sys.argv[1], "r")
 line = f.readline().strip()
 
-tot = 0
+boxes = []
+for i in range(256):
+	boxes.append([])
+
+r = re.compile("(.+)(-|=)(.*)")
+def run_step(step):
+	m = r.match(step)
+	label = m.group(1)
+	box = boxes[hashalgo(label)]
+	command = m.group(2)
+	if command == "-":
+		for i in range(len(box)):
+			if box[i][0] == label:
+				box.pop(i)
+				return
+	elif command == "=":
+		focallength = int(m.group(3))
+		for i in range(len(box)):
+			if box[i][0] == label:
+				box[i] = (label, focallength)
+				return
+		box.append((label, focallength))
+
 for step in line.split(","):
-	tot += hashalgo(step)
+	run_step(step)
+
+tot = 0
+for i in range(len(boxes)):
+	box = boxes[i]
+	for j in range(len(box)):
+		tot += (i + 1) * (j + 1) * box[j][1]
 print(tot)
