803e3c55f9ee86c31090f0294ff01cc2f61d1bb4
Josh Watzman <github@jwatzman.org>
2023-12-20 17:36:33 +0000

    19.1

--- 2023/19/19.py
+++ 2023/19/19.py
@@ -19,12 +19,18 @@ class Widget:
 	a: int
 	s: int
 
+	def __getitem__(self, item):
+		return getattr(self, item)
+
 class AlwaysRule:
 	def __init__(self, r):
 		self.r = get_r(r)
 
 	def run(self, w):
-		return r
+		return self.r
+
+	def __repr__(self):
+		return "Always goto %s" % (self.r)
 
 rule_reg = re.compile("(.+)(<|>)(.+):(.+)")
 class CondRule:
@@ -37,12 +43,15 @@ class CondRule:
 
 	def run(self, w):
 		wv = w[self.field]
-		if op == "<" and vw < self.v:
+		if self.op == "<" and wv < self.v:
 			return self.r
-		elif op == ">" and vw > self.v:
+		elif self.op == ">" and wv > self.v:
 			return self.r
 		else:
 			return None
+
+	def __repr__(self):
+		return "If %s %s %d goto %s" % (self.field, self.op, self.v, self.r)
 
 workflow_reg = re.compile("(.*){(.*)}")
 class Workflow:
@@ -50,8 +59,8 @@ class Workflow:
 		m = workflow_reg.match(line)
 		self.name = m.group(1)
 		self.rules = []
-		for rule_str in m.group(2).split(":"):
-			if rule_str.contains(">") or rule_str.contains("<"):
+		for rule_str in m.group(2).split(","):
+			if ">" in rule_str or "<" in rule_str:
 				self.rules.append(CondRule(rule_str))
 			else:
 				self.rules.append(AlwaysRule(rule_str))
@@ -63,5 +72,34 @@ class Workflow:
 				return res
 		assert False
 
+	def __repr__(self):
+		return "Workflow %s: %s" % (self.name, str(self.rules))
+
+def run_all(workflows, w, state):
+	new_state = workflows[state].run(w)
+	if new_state == True or new_state == False:
+		return new_state
+	else:
+		return run_all(workflows, w, new_state)
+
+workflows = dict()
 f = open(sys.argv[1], "r")
-print(Workflow(f.readlines()[0]))
+lines = f.readlines()
+while len(lines) > 0:
+	line = lines.pop(0).strip()
+	if line == "":
+		break
+	flow = Workflow(line)
+	workflows[flow.name] = flow
+
+tot = 0
+widget_reg = re.compile("{x=(.+),m=(.+),a=(.+),s=(.+)}")
+for line in lines:
+	m = widget_reg.match(line.strip())
+	w = Widget(int(m.group(1)), int(m.group(2)), int(m.group(3)), int(m.group(4)))
+	accept = run_all(workflows, w, "in")
+	if accept:
+		#print(w)
+		tot += w.x + w.m + w.a + w.s
+
+print(tot)
