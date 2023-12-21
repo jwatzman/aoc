e01bc9c3171ca623cf8bea727f37a07da7a9c0b1
Josh Watzman <github@jwatzman.org>
2023-12-21 18:54:07 +0000

    21.1.5

--- 2023/21/21.py
+++ 2023/21/21.py
@@ -2,30 +2,34 @@
 
 import sys
 
-def mk_next(p):
-	return [
+def mk_next(sz, p):
+	candidates = [
 		(p[0]-1, p[1]),
 		(p[0]+1, p[1]),
 		(p[0],   p[1]-1),
 		(p[0],   p[1]+1),
 	]
+	#return map(lambda pp: ((pp[0] + sz[0]) % sz[0], (pp[1] + sz[1]) % sz[1]))
+	#return map(lambda pp: (pp[0] % sz[0], pp[1] % sz[1]), candidates)
+	return candidates
 
-def get_plot(plot, p):
-	return plot[p[0]][p[1]]
+def get_plot(plot, sz, p):
+	return plot[p[0] % sz[0]][p[1] % sz[1]]
 
-def scan(plot, visited, reachable, steps, p):
+def scan(plot, sz, visited, reachable, steps, p):
 	if (p, steps) in visited:
 		return
 	visited.add((p, steps))
-	nxt = mk_next(p)
+	nxt = mk_next(sz, p)
 	if steps == 0:
 		reachable.add(p)
 	else:
-		for nxt_p in filter(lambda pp: get_plot(plot, pp) != "#", nxt):
-			scan(plot, visited, reachable, steps - 1, nxt_p)
+		for nxt_p in filter(lambda pp: get_plot(plot, sz, pp) != "#", nxt):
+			scan(plot, sz, visited, reachable, steps - 1, nxt_p)
 
 f = open(sys.argv[1], "r")
 plot = list(map(lambda l: l.strip(), f.readlines()))
+sz = (len(plot), len(plot[0]))
 
 start = None
 for row in range(len(plot)):
@@ -33,9 +37,9 @@ for row in range(len(plot)):
 		start = (row, plot[row].index("S"))
 		break
 assert start != None
-assert get_plot(plot, start) == "S"
+assert get_plot(plot, sz, start) == "S"
 
 visited = set()
 reachable = set()
-scan(plot, visited, reachable, int(sys.argv[2]), start)
+scan(plot, sz, visited, reachable, int(sys.argv[2]), start)
 print(len(reachable))
