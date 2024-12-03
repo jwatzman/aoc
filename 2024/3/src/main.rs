c5a4ad892073bdbb25a290e68abe6bfdaa692d26
Josh Watzman <github@jwatzman.org>
2024-12-03 11:17:54 -0500

    3.2

--- 2024/3/src/main.rs
+++ 2024/3/src/main.rs
@@ -7,12 +7,21 @@ fn main() {
     let filename = &args[1];
     let contents = fs::read_to_string(filename).expect("Could not read file");
 
-    let re = Regex::new(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)").unwrap();
+    let re = Regex::new(r"(mul\(([0-9]{1,3}),([0-9]{1,3})\))|(do\(\))|(don't\(\))").unwrap();
+    let mut enabled = true;
     let mut res = 0;
-    for (_, [l_str, r_str]) in re.captures_iter(&contents).map(|c| c.extract()) {
-        let l: i32 = l_str.parse().expect("expected number");
-        let r: i32 = r_str.parse().expect("expected number");
-        res += l * r;
+    for c in re.captures_iter(&contents) {
+        let base = c.get(0).unwrap().as_str();
+        if base == "don't()" {
+            enabled = false;
+        } else if base == "do()" {
+            enabled = true;
+        } else if enabled {
+            let l: i32 = c.get(2).unwrap().as_str().parse().expect("expected number");
+            let r: i32 = c.get(3).unwrap().as_str().parse().expect("expected number");
+            res += l * r;
+        }
     }
+
     println!("{res}");
 }
