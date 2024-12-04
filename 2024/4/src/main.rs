0b07d808633fba715a3352637124ad91833c9a6b
Josh Watzman <github@jwatzman.org>
2024-12-04 10:54:45 -0500

    4.2

--- 2024/4/src/main.rs
+++ 2024/4/src/main.rs
@@ -1,23 +1,30 @@
 use std::env;
 use std::fs;
 
-const DELTAS: [i8; 3] = [-1, 0, 1];
+fn find(grid: &Vec<Vec<char>>, loc: &(usize, usize)) -> Option<()> {
+    let up = loc.0.checked_add_signed(-1)?;
+    let dn = loc.0.checked_add_signed(1)?;
+    let lt = loc.1.checked_add_signed(-1)?;
+    let rt = loc.1.checked_add_signed(1)?;
 
-fn find(grid: &Vec<Vec<char>>, cs: &[char], delta: &(i8, i8), loc: &(usize, usize)) -> Option<()> {
-    if cs.len() == 0 {
-        return Some(());
+    let ul = *grid.get(up)?.get(lt)?;
+    let ur = *grid.get(up)?.get(rt)?;
+    let dl = *grid.get(dn)?.get(lt)?;
+    let dr = *grid.get(dn)?.get(rt)?;
+
+    if (ul == ur) && (dl == dr) {
+        if (ul == 'M' && dl == 'S') || (ul == 'S' && dl == 'M') {
+            return Some(());
+        }
     }
 
-    let new_loc = (
-        loc.0.checked_add_signed(delta.0.into())?,
-        loc.1.checked_add_signed(delta.1.into())?,
-    );
-    let c = grid.get(new_loc.0)?.get(new_loc.1)?;
-    if *c == cs[0] {
-        return find(grid, &cs[1..], delta, &new_loc);
-    } else {
-        return None;
+    if (ul == dl) && (ur == dr) {
+        if (ul == 'M' && ur == 'S') || (ul == 'S' && ur == 'M') {
+            return Some(());
+        }
     }
+
+    None
 }
 
 fn main() {
@@ -37,16 +44,8 @@ fn main() {
     for (rn, r) in grid.iter().enumerate() {
         for (cn, c) in r.iter().enumerate() {
             let loc = (rn, cn);
-            for rd in DELTAS {
-                for cd in DELTAS {
-                    if !(rd == 0 && cd == 0)
-                        && *c == 'X'
-                        && find(&grid, &['M', 'A', 'S'], &(rd, cd), &loc).is_some()
-                    {
-                        res += 1;
-                        // println!("{rn}, {cn} + {rd}, {cd}");
-                    }
-                }
+            if *c == 'A' && find(&grid, &loc).is_some() {
+                res += 1;
             }
         }
     }
