7707c3443f0ddb6948e3c4798b46ff74b718c467
Josh Watzman <github@jwatzman.org>
2024-12-08 10:15:04 -0500

    8.2

--- 2024/8/src/main.rs
+++ 2024/8/src/main.rs
@@ -43,9 +43,38 @@ fn parse_input(contents: String) -> City {
     };
 }
 
-fn maybe_insert(city: &City, s: &mut HashSet<Pt>, p: Pt) {
-    if p.0 >= 0 && p.1 >= 0 && p.0 <= city.max.0 && p.1 <= city.max.1 {
-        s.insert(p);
+// https://gist.github.com/victor-iyi/8a84185c1d52419b0d4915a648d5e3e1
+fn gcd(mut n: i32, mut m: i32) -> i32 {
+    if n == 0 {
+        return m;
+    }
+
+    if m == 0 {
+        return n;
+    }
+
+    if n < 0 {
+        n = -n;
+    }
+
+    if m < 0 {
+        m = -m;
+    }
+
+    while m != 0 {
+        if m < n {
+            std::mem::swap(&mut m, &mut n);
+        }
+        m %= n;
+    }
+    return n;
+}
+
+fn scan_antinodes(city: &City, s: &mut HashSet<Pt>, mut r: i32, mut c: i32, dr: i32, dc: i32) {
+    while r >= 0 && c >= 0 && r <= city.max.0 && c <= city.max.1 {
+        s.insert((r, c));
+        r += dr;
+        c += dc;
     }
 }
 
@@ -58,9 +87,14 @@ fn find_antinodes(city: &City) -> HashSet<Pt> {
                 continue;
             }
 
-            let d = (a2.0 - a1.0, a2.1 - a1.1);
-            maybe_insert(city, &mut r, (a2.0 + d.0, a2.1 + d.1));
-            maybe_insert(city, &mut r, (a1.0 - d.0, a1.1 - d.1));
+            let dr_big = a2.0 - a1.0;
+            let dc_big = a2.1 - a1.1;
+            let g = gcd(dr_big, dc_big);
+            let dr = dr_big / g;
+            let dc = dc_big / g;
+
+            scan_antinodes(city, &mut r, a1.0, a1.1, dr, dc);
+            scan_antinodes(city, &mut r, a1.0, a1.1, -dr, -dc);
         }
     }
 
