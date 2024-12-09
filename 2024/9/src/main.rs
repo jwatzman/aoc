c2ec089b785adf4cf0476419b95fbf9729cae54b
Josh Watzman <github@jwatzman.org>
2024-12-09 12:33:30 -0500

    9.2

--- 2024/9/src/main.rs
+++ 2024/9/src/main.rs
@@ -3,16 +3,20 @@ use std::fs;
 
 #[derive(Debug, Clone)]
 enum Block {
-    Free,
-    File(u32),
+    Free(u32),
+    File(u32, u32),
 }
 
 #[allow(dead_code)]
 fn p(v: &Vec<Block>) {
     for b in v {
-        match b {
-            Block::Free => print!("."),
-            Block::File(n) => print!("{n}"),
+        let (len, c) = match b {
+            Block::Free(len) => (*len, '.'),
+            Block::File(len, id) => (*len, id.to_string().chars().nth(0).unwrap()),
+        };
+
+        for _ in 0..len {
+            print!("{c}");
         }
     }
     print!("\n");
@@ -31,48 +35,77 @@ fn parse_input(contents: String) -> Vec<Block> {
         };
 
         let b = if free {
-            Block::Free
+            Block::Free(n)
         } else {
             next_file_id += 1;
-            Block::File(next_file_id - 1)
+            Block::File(n, next_file_id - 1)
         };
         free = !free;
 
-        for _i in 0..n {
-            r.push(b.clone());
-        }
+        r.push(b.clone());
     }
 
     return r;
 }
 
 fn compact(blocks: &Vec<Block>) -> Vec<Block> {
-    let mut res = blocks.clone();
+    let mut blocks = blocks.clone();
 
-    let mut l = 0_usize;
-    let mut r = res.len() - 1;
+    let mut next_file_id = match blocks.last() {
+        Some(Block::File(_len, id)) => *id,
+        _ => panic!("Invalid blocks"),
+    };
 
-    while l < r {
-        match (res[l].clone(), res[r].clone()) {
-            (Block::File(_), _) => l += 1,
-            (Block::Free, Block::Free) => r -= 1,
-            (Block::Free, Block::File(_)) => {
-                res.swap(l, r);
-                l += 1;
-                r -= 1;
+    while next_file_id > 0 {
+        let mut pos = blocks.len() - 1;
+        let len;
+        loop {
+            //dbg!(&blocks[pos]);
+            match blocks[pos] {
+                Block::File(l, id) if id == next_file_id => {
+                    len = l;
+                    break;
+                }
+                _ => pos -= 1,
             }
         }
+
+        let mut target = 0;
+        while target < pos {
+            match blocks[target] {
+                Block::Free(flen) if flen >= len => {
+                    blocks[pos] = Block::Free(len);
+                    blocks[target] = Block::File(len, next_file_id);
+                    if flen > len {
+                        blocks.insert(target + 1, Block::Free(flen - len));
+                    }
+                    break;
+                }
+                _ => target += 1,
+            }
+        }
+
+        next_file_id -= 1;
     }
 
-    return res;
+    return blocks;
 }
 
-fn cksum(blocks: &Vec<Block>) -> usize {
-    let mut r = 0;
-    for (pos, b) in blocks.iter().enumerate() {
+fn cksum(blocks: &Vec<Block>) -> u64 {
+    let mut r = 0_u64;
+    let mut pos = 0;
+    for b in blocks.iter() {
         match b {
-            Block::Free => (),
-            Block::File(id) => r += pos * usize::try_from(*id).unwrap(),
+            Block::Free(len) => pos += len,
+            Block::File(len, id) => {
+                for _ in 0..*len {
+                    match r.checked_add((pos * id).into()) {
+                        Some(n) => r = n,
+                        None => panic!("{} {} {}", r, pos, id),
+                    }
+                    pos += 1;
+                }
+            }
         }
     }
 
@@ -82,9 +115,9 @@ fn cksum(blocks: &Vec<Block>) -> usize {
 fn main() {
     let args: Vec<_> = env::args().collect();
     let blocks = parse_input(fs::read_to_string(&args[1]).unwrap());
-    // p(&blocks);
+    //p(&blocks);
     let compacted = compact(&blocks);
-    // p(&compacted);
+    //p(&compacted);
     let ck = cksum(&compacted);
     println!("{ck}");
 }
