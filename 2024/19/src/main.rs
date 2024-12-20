fbd5fc40a8807c66d5c0a6215cac992f40df34f2
Josh Watzman <github@jwatzman.org>
2024-12-19 22:18:41 -0500

    19.2.5

--- 2024/19/src/main.rs
+++ 2024/19/src/main.rs
@@ -14,24 +14,6 @@ fn parse_input(contents: &String) -> (HashSet<&str>, Vec<&str>) {
     return (towel_patterns, towel_designs);
 }
 
-fn find_one_way<'a>(towel_patterns: &HashSet<&str>, design: &'a str) -> Option<Vec<&'a str>> {
-    if design.is_empty() {
-        return Some(Vec::new());
-    }
-
-    for i in 1..=design.len() {
-        let towel = &design[..i];
-        if towel_patterns.contains(towel) {
-            if let Some(mut v) = find_one_way(towel_patterns, &design[i..]) {
-                v.push(towel);
-                return Some(v);
-            }
-        }
-    }
-
-    return None;
-}
-
 fn count_all_ways<'a>(
     memo: &mut HashMap<&'a str, usize>,
     towel_patterns: &HashSet<&str>,
@@ -57,32 +39,17 @@ fn count_all_ways<'a>(
     return r;
 }
 
-fn reduce<'a>(towel_patterns: &'a HashSet<&str>) -> HashSet<&'a str> {
-    let mut r = towel_patterns.clone();
-    for pattern in towel_patterns.iter() {
-        r.remove(pattern);
-        if find_one_way(&r, pattern).is_none() {
-            r.insert(pattern);
-        }
-    }
-
-    return r;
-}
-
 fn main() {
     let args: Vec<_> = env::args().collect();
     let contents = fs::read_to_string(&args[1]).unwrap();
     let (towel_patterns, towel_designs) = parse_input(&contents);
-    let reduced_towel_patterns = reduce(&towel_patterns);
 
     let mut r = 0;
     let mut memo = HashMap::new();
 
     for design in towel_designs {
-        if find_one_way(&reduced_towel_patterns, design).is_some() {
-            r += count_all_ways(&mut memo, &towel_patterns, design);
-        }
+        r += count_all_ways(&mut memo, &towel_patterns, design);
     }
 
-    println!("{r}");
+    println!("{r} ({})", memo.len());
 }
