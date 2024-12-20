9774918732ba600fccb66f56c4694539c24a59c2
Josh Watzman <github@jwatzman.org>
2024-12-20 11:52:58 -0500

    20.1.5

--- 2024/20/src/main.rs
+++ 2024/20/src/main.rs
@@ -22,13 +22,12 @@ struct Racetrack {
     end: Pt,
 }
 
-#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
 enum Cheat {
-    CanCheat,
-    Cheating(usize),
-    Cheated,
+    CanCheat(usize),
+    Cheating(usize, Pt),
+    Cheated(Pt, Pt),
 }
-const CHEAT_STEPS: usize = 2;
 
 fn parse_input(contents: String) -> Racetrack {
     let mut map = Vec::new();
@@ -71,33 +70,35 @@ fn adj<'a>(
     Direction::ALL.iter().filter_map(move |d| {
         let pt = &state.0 + d.delta();
         let pos = aoc_util::try_get(&racetrack.map, &pt)?;
-        match (*pos, state.1) {
-            (Position::Track, Cheat::CanCheat) => Some((pt, Cheat::CanCheat)),
-            (Position::Track, _) => Some((pt, Cheat::Cheated)),
-            (Position::Wall, Cheat::CanCheat) => Some((pt, Cheat::Cheating(CHEAT_STEPS - 1))),
-            (Position::Wall, Cheat::Cheating(n)) if n > 1 => Some((pt, Cheat::Cheating(n - 1))),
-            (Position::Wall, _) => None,
+        match (*pos, state.1.clone()) {
+            (Position::Track, Cheat::CanCheat(_)) => Some((pt, state.1.clone())),
+            (Position::Track, Cheat::Cheating(_, start)) => {
+                Some((pt.clone(), Cheat::Cheated(start, pt.clone())))
+            }
+            (Position::Track, Cheat::Cheated(_, _)) => Some((pt, state.1.clone())),
+            (Position::Wall, Cheat::CanCheat(n)) => {
+                Some((pt.clone(), Cheat::Cheating(n - 1, pt.clone())))
+            }
+            (Position::Wall, Cheat::Cheating(n, start)) if n > 1 => {
+                Some((pt, Cheat::Cheating(n - 1, start)))
+            }
+            (Position::Wall, Cheat::Cheating(_, _)) => None,
+            (Position::Wall, Cheat::Cheated(_, _)) => None,
         }
     })
 }
 
-fn solve(racetrack: &Racetrack, enable_cheats: bool) -> usize {
+fn solve(racetrack: &Racetrack, init_cheat: Cheat) -> (usize, (Pt, Pt)) {
     let mut pq = priority_queue::PriorityQueue::new();
     let mut visited = HashSet::new();
     let mut costs = HashMap::new();
+    let mut prevs = HashMap::new();
 
     let approx_sz = racetrack.map.len() * racetrack.map.len();
     visited.reserve(approx_sz);
     costs.reserve(approx_sz);
 
-    let init_state = (
-        racetrack.start.clone(),
-        if enable_cheats {
-            Cheat::CanCheat
-        } else {
-            Cheat::Cheated
-        },
-    );
+    let init_state = (racetrack.start.clone(), init_cheat);
 
     costs.insert(init_state.clone(), 0);
     pq.push(init_state, Reverse(0));
@@ -105,7 +106,13 @@ fn solve(racetrack: &Racetrack, enable_cheats: bool) -> usize {
     while let Some((state, _)) = pq.pop() {
         let cost = *costs.get(&state).unwrap();
         if state.0 == racetrack.end {
-            return cost;
+            return (
+                cost,
+                match state.1 {
+                    Cheat::Cheated(start, end) => (start, end),
+                    _ => panic!(),
+                },
+            );
         }
 
         for next in adj(racetrack, &state) {
@@ -115,11 +122,13 @@ fn solve(racetrack: &Racetrack, enable_cheats: bool) -> usize {
             match prev_tot_cost_opt {
                 None => {
                     costs.insert(next.clone(), tot_cost);
+                    prevs.insert(next.clone(), state.clone());
                     pq.push(next, Reverse(tot_cost));
                 }
                 Some(prev_tot_cost) => {
                     if tot_cost < *prev_tot_cost {
                         *prev_tot_cost = tot_cost;
+                        prevs.insert(next.clone(), state.clone());
                         pq.push(next, Reverse(tot_cost));
                     }
                 }
@@ -135,7 +144,14 @@ fn solve(racetrack: &Racetrack, enable_cheats: bool) -> usize {
 fn main() {
     let args: Vec<_> = env::args().collect();
     let racetrack = parse_input(fs::read_to_string(&args[1]).unwrap());
-    let baseline = solve(&racetrack, false);
-    let fastest = solve(&racetrack, true);
+
+    let dummy_pt = Pt { row: 0, col: 0 };
+    let (baseline, _) = solve(
+        &racetrack,
+        Cheat::Cheated(dummy_pt.clone(), dummy_pt.clone()),
+    );
+
+    let (fastest, cheat_used) = solve(&racetrack, Cheat::CanCheat(2));
     println!("{}", baseline - fastest);
+    dbg!(cheat_used);
 }
