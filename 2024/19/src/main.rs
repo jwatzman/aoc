0b71a4d90237ac42a0de676fa5e1c9a13eceae6d
Josh Watzman <github@jwatzman.org>
2024-12-19 22:01:22 -0500

    19.1.5

--- 2024/19/src/main.rs
+++ 2024/19/src/main.rs
@@ -13,28 +13,27 @@ fn parse_input(contents: &String) -> (HashSet<&str>, Vec<&str>) {
     return (towel_patterns, towel_designs);
 }
 
-fn can_make_design(towel_patterns: &HashSet<&str>, design: &str) -> bool {
+fn can_make_design(towel_patterns: &HashSet<&str>, design: &str) -> usize {
     if design.is_empty() {
-        return true;
+        return 1;
     }
 
+    let mut r = 0;
     for i in 1..=design.len() {
         let towel = &design[..i];
         if towel_patterns.contains(towel) {
-            if can_make_design(towel_patterns, &design[i..]) {
-                return true;
-            }
+            r += can_make_design(towel_patterns, &design[i..]);
         }
     }
 
-    return false;
+    return r;
 }
 
-fn reduce(towel_patterns: HashSet<&str>) -> HashSet<&str> {
+fn reduce<'a>(towel_patterns: &'a HashSet<&str>) -> HashSet<&'a str> {
     let mut r = towel_patterns.clone();
-    for pattern in towel_patterns.into_iter() {
+    for pattern in towel_patterns.iter() {
         r.remove(pattern);
-        if !can_make_design(&r, pattern) {
+        if can_make_design(&r, pattern) == 0 {
             r.insert(pattern);
         }
     }
@@ -46,13 +45,12 @@ fn main() {
     let args: Vec<_> = env::args().collect();
     let contents = fs::read_to_string(&args[1]).unwrap();
     let (towel_patterns, towel_designs) = parse_input(&contents);
-    let towel_patterns = reduce(towel_patterns);
+    let reduced_towel_patterns = reduce(&towel_patterns);
 
     let mut r = 0;
     for design in towel_designs {
-        let can_make = can_make_design(&towel_patterns, design);
-        if can_make {
-            r += 1;
+        if can_make_design(&reduced_towel_patterns, design) > 0 {
+            r += can_make_design(&towel_patterns, design);
         }
     }
 
