410265993534bc63d6b0436a95670a1e6171966d
Josh Watzman <github@jwatzman.org>
2024-12-21 22:12:32 -0500

    21.0.5

---
+++ 2024/21/src/main.rs
@@ -0,0 +1,193 @@
+use std::env;
+use std::fmt::Display;
+use std::fs;
+use std::iter;
+
+type RC = i8;
+
+trait Button: Copy {
+    fn row(&self) -> RC;
+    fn col(&self) -> RC;
+
+    const HOLE: (RC, RC);
+    const START: Self;
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum NumericButton {
+    Zero,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Activate,
+}
+
+impl Button for NumericButton {
+    fn row(&self) -> RC {
+        match *self {
+            Self::Seven | Self::Eight | Self::Nine => 0,
+            Self::Four | Self::Five | Self::Six => 1,
+            Self::One | Self::Two | Self::Three => 2,
+            Self::Zero | Self::Activate => 3,
+        }
+    }
+
+    fn col(&self) -> RC {
+        match *self {
+            Self::Seven | Self::Four | Self::One => 0,
+            Self::Eight | Self::Five | Self::Two | Self::Zero => 1,
+            Self::Nine | Self::Six | Self::Three | Self::Activate => 2,
+        }
+    }
+
+    const HOLE: (RC, RC) = (3, 0);
+    const START: Self = NumericButton::Activate;
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum DirectionalButton {
+    Up,
+    Down,
+    Left,
+    Right,
+    Activate,
+}
+
+impl Display for DirectionalButton {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let c = match *self {
+            Self::Up => '^',
+            Self::Down => 'v',
+            Self::Left => '<',
+            Self::Right => '>',
+            Self::Activate => 'A',
+        };
+        write!(f, "{c}")
+    }
+}
+
+impl Button for DirectionalButton {
+    fn row(&self) -> RC {
+        match *self {
+            Self::Up | Self::Activate => 0,
+            Self::Left | Self::Down | Self::Right => 1,
+        }
+    }
+
+    fn col(&self) -> RC {
+        match *self {
+            Self::Left => 0,
+            Self::Up | Self::Down => 1,
+            Self::Activate | Self::Right => 2,
+        }
+    }
+
+    const HOLE: (RC, RC) = (0, 0);
+    const START: Self = DirectionalButton::Activate;
+}
+
+fn parse_input(contents: String) -> Vec<(Vec<NumericButton>, usize)> {
+    let mut codes = Vec::new();
+
+    for line in contents.split('\n') {
+        if line.is_empty() {
+            continue;
+        }
+
+        let mut code = Vec::new();
+        for c in line.chars() {
+            code.push(match c {
+                '0' => NumericButton::Zero,
+                '1' => NumericButton::One,
+                '2' => NumericButton::Two,
+                '3' => NumericButton::Three,
+                '4' => NumericButton::Four,
+                '5' => NumericButton::Five,
+                '6' => NumericButton::Six,
+                '7' => NumericButton::Seven,
+                '8' => NumericButton::Eight,
+                '9' => NumericButton::Nine,
+                'A' => NumericButton::Activate,
+                _ => panic!(),
+            })
+        }
+
+        codes.push((code, line[0..3].parse().unwrap()));
+    }
+
+    return codes;
+}
+
+fn expand<T: Button>(code: &Vec<T>) -> Vec<DirectionalButton> {
+    let mut cur_loc = T::START;
+    let mut out = Vec::new();
+
+    for next_loc in code {
+        let row_diff = next_loc.row() - cur_loc.row();
+        let col_diff = next_loc.col() - cur_loc.col();
+
+        let row_presses = iter::repeat(if row_diff < 0 {
+            DirectionalButton::Up
+        } else {
+            DirectionalButton::Down
+        })
+        .take(row_diff.abs().try_into().unwrap());
+
+        let col_presses = iter::repeat(if col_diff < 0 {
+            DirectionalButton::Left
+        } else {
+            DirectionalButton::Right
+        })
+        .take(col_diff.abs().try_into().unwrap());
+
+        if cur_loc.col() == T::HOLE.1 && next_loc.row() == T::HOLE.0 {
+            out.extend(col_presses);
+            out.extend(row_presses);
+        } else {
+            out.extend(row_presses);
+            out.extend(col_presses);
+        }
+
+        out.push(DirectionalButton::Activate);
+        cur_loc = *next_loc;
+    }
+
+    return out;
+}
+
+fn expand_all(code: &Vec<NumericButton>) -> Vec<DirectionalButton> {
+    let lv1 = expand(code);
+    print_buttons(&lv1);
+    let lv2 = expand(&lv1);
+    print_buttons(&lv2);
+    let lv3 = expand(&lv2);
+    print_buttons(&lv3);
+    println!("{}", lv3.len());
+    return lv3;
+}
+
+fn print_buttons(buttons: &Vec<DirectionalButton>) {
+    for button in buttons {
+        print!("{button}");
+    }
+    print!("\n");
+}
+
+fn main() {
+    let args: Vec<_> = env::args().collect();
+    let codes = parse_input(fs::read_to_string(&args[1]).unwrap());
+
+    let mut r = 0;
+    for code in &codes {
+        let long = expand_all(&code.0);
+        r += long.len() * code.1;
+    }
+
+    println!("{r}");
+}
