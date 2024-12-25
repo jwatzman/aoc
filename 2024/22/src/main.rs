6b278b297e6f58a45f4e6d83fd4fa60e024d9fc2
Josh Watzman <github@jwatzman.org>
2024-12-24 22:20:25 -0500

    22.2

--- 2024/22/src/main.rs
+++ 2024/22/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
 use std::env;
 use std::fs;
 
@@ -21,18 +23,54 @@ fn evolve(mut secret: u64) -> u64 {
     return secret;
 }
 
+fn prices(mut secret: u64) -> Vec<i8> {
+    let mut r = Vec::new();
+    r.reserve(2001);
+
+    for _ in 0..2001 {
+        r.push(i8::try_from(secret % 10).unwrap());
+        secret = evolve(secret);
+    }
+
+    return r;
+}
+
+fn tally(tot: &mut HashMap<(i8, i8, i8, i8), u64>, init_secret: u64) {
+    let mut seen = HashSet::new();
+    let prices = prices(init_secret);
+
+    for i in 4..prices.len() {
+        let a = prices[i - 4];
+        let b = prices[i - 3];
+        let c = prices[i - 2];
+        let d = prices[i - 1];
+        let e = prices[i];
+
+        let k = (b - a, c - b, d - c, e - d);
+
+        if seen.insert(k) {
+            *tot.entry(k).or_insert(0) += u64::try_from(e).unwrap();
+        }
+    }
+}
+
 fn main() {
     let args: Vec<_> = env::args().collect();
     let init_secrets = parse_input(fs::read_to_string(&args[1]).unwrap());
 
-    let mut r = 0;
+    let mut tot = HashMap::new();
     for init_secret in &init_secrets {
-        let mut secret = *init_secret;
-        for _ in 0..2000 {
-            secret = evolve(secret);
-        }
-        r += secret;
+        tally(&mut tot, *init_secret);
     }
 
-    println!("{r}")
+    let mut best_n = u64::MIN;
+    let mut best_seq = None;
+    for (seq, n) in tot {
+        if n > best_n {
+            best_n = n;
+            best_seq = Some(seq);
+        }
+    }
+
+    println!("{best_n} {best_seq:?}");
 }
